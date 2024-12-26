27ecfa09b19da1dd6ca057805641959d6ec4aed1
Yuan Lyu <lyuyuan92@gmail.com>
2024-12-26 14:32:38 -0500

    Clean up my mess

--- editor/nvim/lua/plugins/git.lua
+++ editor/nvim/lua/plugins/git.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"mhinz/vim-signify",
+		config = function()
+			-- hunk mappings
+			vim.keymap.set('n', '<leader>hd', '<cmd>SignifyDiff<cr>')
+			vim.keymap.set('n', '<leader>hp', '<cmd>SignifyHunkDiff<cr>')
+			vim.keymap.set('n', '<leader>hu', '<cmd>SignifyHunkUndo<cr>')
+		end
 	},
 	-- auto-cd to root of git project
 	{
--- editor/nvim/lua/plugins/gui.lua
+++ editor/nvim/lua/plugins/gui.lua
@@ -6,6 +6,8 @@ return {
 			vim.g.loaded_netrw = 1
 			vim.g.loaded_netrwPlugin = 1
 			require('nvim-tree').setup()
+			-- quick toggle
+			vim.keymap.set('n', '<leader>nt', '<cmd>NvimTreeToggle<cr>')
 		end
 	},
 	{
