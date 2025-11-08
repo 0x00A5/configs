0c54cf2b24f9b2487ebd5a130e0b1055b3df9251
Yuan Lyu <lyuyuan92@gmail.com>
2025-11-07 20:51:12 -0500

    devcontrainer refresh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,9 @@
 # Bootstrap script for `devcontrainer up`
 
 # install packages
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-fish -c "omf install bobthefish"
 
 # use stow to create symlink
 mkdir ~/.config
-cd ~/dotfiles
+cd ~/dotfiles || exit
 stow -t ~ shell
 stow -t ~ editor
