40738e3f1447a8e1c6ba45a9c06bbc2648bfd082
Yuan Lyu <lyuyuan92@gmail.com>
2025-11-25 22:27:45 -0500

    Fish

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -2,4 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -x MIRRORD_LAYER_FILE_MACOS_ARM64 "../../../target/aarch64-apple-darwin/debug/libmirrord_layer.dylib"
+source ~/.local/bin/env
+
 set -gx GPG_TTY (tty)
