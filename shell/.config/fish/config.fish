318e89451d7107c24335660859b737fb9f1ca205
Yuan Lyu <lyuyuan92@gmail.com>
2025-12-25 23:47:28 -0500

    Fix aerospace launch alacritty

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -3,6 +3,5 @@ if status is-interactive
 end
 
 set -x MIRRORD_LAYER_FILE_MACOS_ARM64 "../../../target/aarch64-apple-darwin/debug/libmirrord_layer.dylib"
-source ~/.local/bin/env
 
 set -gx GPG_TTY (tty)
