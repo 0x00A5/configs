9ce947ac9c67c9016e9df3e17dfd50f7b73f9b19
Yuan Lyu <lyuyuan92@gmail.com>
2026-01-12 22:42:24 -0500

    Fix mirrord lib env var

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x MIRRORD_LAYER_FILE_MACOS_ARM64 "../../../target/aarch64-apple-darwin/debug/libmirrord_layer.dylib"
+set -x MIRRORD_LAYER_FILE_MACOS_ARM64 "/usr/local/share/libmirrord_layer.dylib"
 
 set -gx GPG_TTY (tty)
