d4b86c1c1e0947444a49a983f306b42f81b60ca3
Yuan Lyu <lyuyuan92@gmail.com>
2026-01-25 10:33:51 -0500

    Bye bye XFCE

---
+++ gui/.config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.3; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar main &
+done
+
--- gui/bin/i3-workspace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# ~/bin/i3-workspaces.sh
-i3-msg -t get_workspaces | jq -r '.[] | if .focused then "[" + .name + "]" else .name end' | tr '\n' ' '
