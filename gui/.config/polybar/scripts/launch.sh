68843c90f6b73a5e4f178cde1ff76f50340c49db
Yuan Lyu <lyuyuan92@gmail.com>
2026-02-22 00:11:27 -0500

    polybar launch script

--- gui/.config/polybar/scripts/launch.sh
+++ gui/.config/polybar/scripts/launch.sh
@@ -3,7 +3,8 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.3; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar main &
-done
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#  MONITOR=$m polybar main &
+polybar main &
+#done
 
